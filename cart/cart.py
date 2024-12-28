e0a15063f387e0d70ab525815f925945e3de2fe1
pavlo <frontbastard@gmail.com>
2024-12-28 23:14:33 +0100

    Create order models

--- cart/cart.py
+++ cart/cart.py
@@ -20,6 +20,34 @@ class Cart:
 
         self.cart = cart
 
+    def __iter__(self):
+        """
+        Iterate over the items in the cart and get the products from the
+        database
+        :return:
+        """
+
+        product_ids = self.cart.keys()
+        # get the product objects and add them to the cart
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]["product"] = product
+
+        for item in cart.values():
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["quantity"]
+            yield item
+
+    def __len__(self):
+        """
+        Count all items in the cart.
+        :return:
+        """
+
+        return sum(item["quantity"] for item in self.cart.values())
+
     def add(self, product, quantity=1, override_quantity=False):
         """
         Add a product to the cart or update its quantity.
@@ -64,37 +92,9 @@ class Cart:
     def get_total_price(self):
         return sum(
             Decimal(item["price"]) * item["quantity"]
-            for item in self.cart.values()
+                for item in self.cart.values()
         )
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.save()
-
-    def __iter__(self):
-        """
-        Iterate over the items in the cart and get the products from the
-        database
-        :return:
-        """
-
-        product_ids = self.cart.keys()
-        # get the product objects and add them to the cart
-        products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
-
-        for product in products:
-            cart[str(product.id)]["product"] = product
-
-        for item in cart.values():
-            item["price"] = Decimal(item["price"])
-            item["total_price"] = item["price"] * item["quantity"]
-            yield item
-
-    def __len__(self):
-        """
-        Count all items in the cart.
-        :return:
-        """
-
-        return sum(item["quantity"] for item in self.cart.values())
