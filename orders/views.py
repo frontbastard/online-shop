d670c935b1de3a005991e8a215f45c79c7038947
pavlo <frontbastard@gmail.com>
2025-01-01 14:57:56 +0100

    Create the payment app and build the payment process

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from cart.cart import Cart
 from cart.models import OrderItem
@@ -27,11 +27,10 @@ def order_create(request):
             cart.clear()
             # launch async task
             order_created.delay(order.id)
-            return render(
-                request,
-                "orders/order/created.html",
-                {"order": order}
-            )
+            # set the order in the session
+            request.session["order_id"] = order.id
+            # redirect for payment
+            return redirect("payment:process")
     else:
         form = OrderCreateForm()
     return render(
