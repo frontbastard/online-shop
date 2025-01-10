cd9d9ccef7697f66f17f57edc8c596429962bd8e
pavlo <frontbastard@gmail.com>
2025-01-10 13:55:23 +0100

    Adapt the Cart class for coupons

--- cart/cart.py
+++ cart/cart.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 
+from coupons.models import Coupon
 from online_shop import settings
 from shop.models import Product
 
@@ -19,6 +20,8 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+        # store current applied coupon
+        self.coupon_id = self.session.get("coupon_id")
 
     def __iter__(self):
         """
@@ -47,6 +50,25 @@ class Cart:
         """
 
         return sum(item["quantity"] for item in self.cart.values())
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (
+                self.coupon.discount / Decimal(100)
+            ) * self.get_total_price()
+        return Decimal(0)
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
 
     def add(self, product, quantity=1, override_quantity=False):
         """
