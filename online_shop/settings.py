3e162e04a75e272c591951340fca482e9e435e63
pavlo <frontbastard@gmail.com>
2025-01-14 22:08:32 +0100

    Translate the languages

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 
 from decouple import config
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,8 +110,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en"
 LANGUAGES = [
-    ("en-us", "English"),
-    ("es-es", "Spanish"),
+    ("en-us", _("English")),
+    ("es-es", _("Spanish")),
 ]
 LOCALE_PATHS = [
     BASE_DIR / "locale",
