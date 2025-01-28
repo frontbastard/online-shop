38e638e7a1602d1d7be224c59bffe8055fce2d25
pavlo <frontbastard@gmail.com>
2025-01-28 21:58:44 +0100

    Adapt product views for translations

--- shop/views.py
+++ shop/views.py
@@ -11,7 +11,12 @@ def product_list(request, category_slug=None):
     products = Product.objects.filter(available=True)
 
     if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
+        language = request.LANGUAGE_CODE
+        category = get_object_or_404(
+            Category,
+            translations__language_code=language,
+            translations__slug=category_slug,
+        )
         products = products.filter(category=category)
 
     return render(
@@ -26,8 +31,14 @@ def product_list(request, category_slug=None):
 
 
 def product_detail(request, id, slug):
+    language = request.LANGUAGE_CODE
     product = get_object_or_404(
-        Product, id=id, slug=slug, available=True,
+        Product,
+        id=id,
+        translations__language_code=language,
+        translations__slug=slug,
+        slug=slug,
+        available=True,
     )
     cart_product_form = CartAddProductForm()
     r = Recommender()
